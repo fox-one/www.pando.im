723f3baa8197f97c17a38b6c4da58c0f9e59086f
Lyric <5h3ll3x@gmail.com>
2021-07-30 17:55:41 +0900

    feat: new icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="footer">
     <div class="footer-inner d-flex align-center mt-4">
-      <div class="social-icons mr-4">
-        <v-icon color="black">{{ $icons.mdiTwitter }}</v-icon>
-      </div>
-      <div class="f-caption f-greyscale-3 links">
-        <a href="https://docs.pando.im/" target="_blank">Docs</a>
-        <span class="mx-2">·</span>
-        <a href="https://blog.pando.im/" target="_blank">Blog</a>
-        <span class="mx-2">·</span>
-        <a href="https://discord.gg/CNS4QQ6w5u" target="_blank">Discord</a>
-      </div>
-      <v-spacer />
-      <div class="f-caption f-greyscale-3">Copyright © 2021 Pando</div>
+      <v-row>
+        <v-col cols="12" md="9">
+          <div class="d-flex">
+            <a
+              v-for="link in links"
+              :key="link.label"
+              :title="link.label"
+              :src="link.url"
+              class="social-icons mr-4"
+              target="_blank"
+            >
+              <v-img
+                :height="32"
+                :width="32"
+                :src="link.icon"
+                :alt="link.alt"
+              />
+            </a>
+          </div>
+        </v-col>
+        <v-col cols="12" md="3">
+          <div class="f-caption f-greyscale-3 text-right">
+            Copyright © 2021 Pando
+          </div>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
@@ -26,6 +40,35 @@ class ProductItem extends Vue {
 
   get productStyles() {
     return { "background-color": this.product?.bg_color || "#fafafa" };
+  }
+
+  get links() {
+    return [
+      {
+        label: "Docs",
+        alt: "Pando's documents for users and developers",
+        icon: require("~/assets/images/meta-docs.svg"),
+        url: "https://docs.pando.im",
+      },
+      {
+        label: "Blog",
+        alt: "Pando's official blog",
+        icon: require("~/assets/images/meta-blog.svg"),
+        url: "https://blog.pando.im",
+      },
+      {
+        label: "Twitter",
+        alt: "Pando's official Twitter account",
+        icon: require("~/assets/images/meta-twitter.svg"),
+        url: "https://twitter.com",
+      },
+      {
+        label: "Discord",
+        alt: "Pando's official discord server",
+        icon: require("~/assets/images/meta-discord.svg"),
+        url: "https://discord.gg/CNS4QQ6w5u",
+      },
+    ];
   }
 }
 export default ProductItem;
